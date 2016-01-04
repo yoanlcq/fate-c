32bc1d342f16a44a3cae1bc7f12a61b272db94ff
yoanlcq <yoanlecoq.io@gmail.com>
2016-01-04 16:08:45 +0100

    .

--- ci/travis/before_install-osx.sh
+++ ci/travis/before_install-osx.sh
@@ -2,13 +2,14 @@
 
 brew update;
 brew tap homebrew/versions;
-sudo brew install gcc49;
+brew install gcc49;
+brew link --overwrite gcc49;
 cd /usr/local/bin
-sudo ln -sf gcc-4.9 gcc
+sudo ln -vsf gcc-4.9 gcc
 cd ~
 wget https://www.libsdl.org/release/SDL2-2.0.4.dmg;
 # From http://commandlinemac.blogspot.fr/2008/12/installing-dmg-application-from-command.html
-hdiutil mount SDL-2.0.4.dmg
-sudo cp -R "/Volumes/SDL-2.0.4/SDL2.framework" /Library/Frameworks
-hdiutil unmount "/Volumes/SDL-2.0.4/"
+hdiutil mount SDL2-2.0.4.dmg
+sudo cp -R "/Volumes/SDL2-2.0.4/SDL2.framework" /Library/Frameworks
+hdiutil unmount "/Volumes/SDL2-2.0.4/"
 
