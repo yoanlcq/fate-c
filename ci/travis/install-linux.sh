95187d08dd7aa6143a9ad52e8d7a7ec9f9d61e0c
yoanlcq <yoanlecoq.io@gmail.com>
2016-06-16 18:44:14 +0200

    Update .travis.yml to force install packages.

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install -y libsdl2-dev gcc-4.9;
+sudo apt-get install -y --force-yes libsdl2-dev gcc-4.9;
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60;
 
