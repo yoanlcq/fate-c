c3b642f6b83bee284ec3682f049547f5d2b2784d
yoanlcq <yoanlecoq.io@gmail.com>
2016-06-16 23:30:29 +0200

    Commented the inclusion of tpipv6.h.

--- src/fate/ipv6.c
+++ src/fate/ipv6.c
@@ -15,9 +15,11 @@
 #include <windows.h>
 #include <winsock2.h>
 #include <Ws2tcpip.h>
+/*
 #if !defined(IPPROTO_IPV6) || (_WIN32_WINNT == 0x0500)
 #include <tpipv6.h>
 #endif
+*/
 #define FE_IPV6_SYSERR(code) WSA##code
 #define FE_IPV6_GETLASTERROR() WSAGetLastError()
 typedef int socklen_t;
