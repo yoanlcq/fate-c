ee476c977b722fe523d79041c6da399050a25c05
yoanlcq <yoanlecoq.io@gmail.com>
2016-06-17 10:00:17 +0200

    .

--- make/cl.mk
+++ make/cl.mk
@@ -4,7 +4,7 @@ CCDEBUGFLAGS = $(CCFLAGS) /Ob2 /Zi /DEBUG /DFATE_DEBUG_BUILD \
 			   /DFATE_ENABLE_TRACING $(GLEWFLAGS)
 CCRELEASEFLAGS = $(CCFLAGS) /O2 /Ot /Ox /GL /Gw /DNDEBUG $(GLEWFLAGS)
 LDLIBS = SDL2.lib SDL2main.lib opengl32.lib d3d10.lib dxguid.lib \
-		 gdi32.lib user32.lib Kernel32.lib DbgHelp.lib ws2_32.lib
+		 gdi32.lib user32.lib Kernel32.lib DbgHelp.lib ws2_32.lib libintl.lib
 CCOBJ = /c
 CCOUT_OBJ = /Fo
 CCOUT_EXE = /Fe
