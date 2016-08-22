2b697daab8d82732b6f2fffbc78ae366e42058e0
Yoan <yoanlecoq.io@gmail.com>
2016-08-22 15:53:26 +0200

    Update src.h

--- include/fate/gl/src.h
+++ include/fate/gl/src.h
@@ -1,9 +1,12 @@
 #ifndef FE_GL_SRC
 #define FE_GL_SRC
 
+#include <fate/gl.h>
 #include <fate/timestamp.h>
+
 fe_timestamp fe_gl_src_get_build_timestamp(void);
-extern const char *const fe_gl_src_tri_330_vert;
-extern const char *const fe_gl_src_tri_330_frag;
+void fe_gl_src_before_linking(const fe_gl_shader_source_set *ss, GLuint prog, fe_timestamp last_build_time);
+extern const char *const fe_gl_src_tri_130_vert;
+extern const char *const fe_gl_src_tri_130_frag;
 
 #endif /* FE_GL_SRC */
